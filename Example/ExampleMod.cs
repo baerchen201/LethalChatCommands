fd196e5fa20278844cc748df4543c939fec08779
baer1 <videocreator@outlook.de>
2025-05-19 00:04:35 +0200

    Update example mod (add more comments, Version 1.0.1)

--- Example/ExampleMod.cs
+++ Example/ExampleMod.cs
@@ -13,108 +13,94 @@ namespace ExampleMod;
 [BepInDependency("baer1.ChatCommandAPI", BepInDependency.DependencyFlags.HardDependency)]
 public class ExampleMod : BaseUnityPlugin
 {
-    public static ExampleMod Instance { get; private set; } = null!;
     internal static new ManualLogSource Logger { get; private set; } = null!;
     internal static Harmony? Harmony { get; set; }
 
     private void Awake()
     {
         Logger = base.Logger;
-        Instance = this;
 
         Patch();
 
-        _ = new StatusCommand();
+        _ = new StatusCommand(); // Initialize command once to register it
 
         Logger.LogInfo($"{MyPluginInfo.PLUGIN_GUID} v{MyPluginInfo.PLUGIN_VERSION} has loaded!");
     }
 
-    internal static void Patch()
+    private static void Patch()
     {
-        Harmony ??= new Harmony(MyPluginInfo.PLUGIN_GUID);
+        Harmony ??= new Harmony(MyPluginInfo.PLUGIN_GUID); // Create Harmony patcher if it doesn't exist
 
         Logger.LogDebug("Patching...");
 
-        Harmony.PatchAll();
+        Harmony.PatchAll(); // Apply all Harmony patches (See below StartPatch)
 
         Logger.LogDebug("Finished patching!");
     }
-
-    internal static void Unpatch()
-    {
-        Logger.LogDebug("Unpatching...");
-
-        Harmony?.UnpatchSelf();
-
-        Logger.LogDebug("Finished unpatching!");
-    }
 }
 
-public class StatusCommand : Command
+public class StatusCommand : Command // Create command subclass
 {
-    public override string Name => "Status";
-    public override string? Description =>
-        "Displays some useful information about the current game";
+    public override string Name => "Status"; // Set command name (default would have been "StatusCommand")
+    public override string[] Commands => [Name.ToLower()]; // While this is not necessary (the Chat Command API is case-insensitive), /status looks better than /Status
+    public override string Description => "Displays some useful information about the current game"; // Command description for /help
 
     public override bool Invoke(string[] args, Dictionary<string, string> kwargs, out string error)
     {
-        error = null!;
-        StringBuilder sb = new StringBuilder($"Current game: ");
-        var lobby = GameNetworkManager.Instance.currentLobby;
-        if (lobby == null)
-            sb.Append("unknown");
-        else
-            sb.Append(lobby.Value.GetData("name"));
-        sb.Append("\n");
+        error = null!; // No error is expected, no need to set error string (exceptions are handled automatically)
 
+        StringBuilder sb = new StringBuilder("Current game: "); // Create StringBuilder, multiple chat messages are not recommended (they could get cut off because of the chat length limit, 4 messages in vanilla)
+
+        // Display information about the lobby you're playing in
+        var lobby = GameNetworkManager.Instance.currentLobby;
+        // The comment below shuts up my IDE because I want this to be simple and easily readable
+        // ReSharper disable once ConvertIfStatementToConditionalTernaryExpression
+        if (lobby == null)
+            sb.Append("unknown"); // No lobby set, either you're playing without steam (LAN mode) or the game is bugged
+        else
+            sb.Append(lobby.Value.GetData("name")); // Get steam lobby name
+        sb.Append("\n"); // Add newline after lobby name
+
+        // List all players and if they are dead
         sb.Append("Players:\n");
-        foreach (var i in StartOfRound.Instance.allPlayerScripts)
+        foreach (var player in StartOfRound.Instance.allPlayerScripts)
         {
             ExampleMod.Logger.LogDebug(
-                $"{(i == null ? "null" : $"{i}\n#{i.playerClientId} {i.playerUsername}")}\nisPlayerControlled: {(i == null ? "null" : i.isPlayerControlled)}\nisPlayerDead: {(i == null ? "null" : i.isPlayerDead)}\ndisconnectedMidGame: {(i == null ? "null" : i.disconnectedMidGame)}"
+                $"{(player == null ? "null" : $"{player}\n#{player.playerClientId} {player.playerUsername}")}\nisPlayerControlled: {(player == null ? "null" : player.isPlayerControlled)}\nisPlayerDead: {(player == null ? "null" : player.isPlayerDead)}\ndisconnectedMidGame: {(player == null ? "null" : player.disconnectedMidGame)}"
             );
-            if (i == null || !Utils.IsPlayerControlled(i))
+
+            if (!Utils.IsPlayerControlled(player)) // Check if player object is actually a real player
                 continue;
-            sb.Append(i.isPlayerDead ? "<color=#ff0000>" : "");
-            sb.Append($" #{i.playerClientId} {i.playerUsername}");
-            sb.Append(i.isHostPlayerObject ? " (HOST)\n" : "\n");
-            sb.Append(i.isPlayerDead ? "</color>" : "");
+
+            sb.Append(player!.isPlayerDead ? "<color=#ff0000>" : ""); // Show player name in red if player is dead
+            sb.Append($" #{player.playerClientId} {player.playerUsername}"); // Player id and username
+            sb.Append(player.isHostPlayerObject ? " (HOST)\n" : "\n"); // If the player is the lobby host, add "(HOST)"
+            sb.Append(player.isPlayerDead ? "</color>" : ""); // End of red color (if player is dead)
         }
 
-        TimeSpan timePlaying = DateTime.Now - _connectTime;
-        ExampleMod.Logger.LogDebug(
-            $"timePlaying: {timePlaying}\nconnectTime: {_connectTime}\nNow: {DateTime.Now}"
-        );
-        ExampleMod.Logger.LogDebug(timePlaying.Seconds);
+        // Display time you have spent in the current lobby
+        TimeSpan timePlaying = DateTime.Now - connectTime; // Get the time difference to the time you connected to the current game
         sb.Append(
             $"Time playing: {(int)timePlaying.TotalHours:D2}:{timePlaying.Minutes:D2}:{timePlaying.Seconds:D2}"
         );
 
-        Print(sb.ToString());
+        ChatCommandAPI.ChatCommandAPI.Print(sb.ToString()); // Print generated output to chat
         return true;
     }
 
-    private static DateTime _connectTime;
+    // You can use static properties because the command should only ever be instantiated once
+    private static DateTime connectTime = new(0); // Initialize date-time to prevent null errors
 
-    [HarmonyPatch(typeof(NetworkManager), nameof(NetworkManager.StartClient))]
-    internal class ClientStartPatch
+    [HarmonyPatch(typeof(NetworkManager), nameof(NetworkManager.StartClient))] // On join lobby
+    [HarmonyPatch(typeof(NetworkManager), nameof(NetworkManager.StartHost))] // On start hosting lobby
+    internal class StartPatch
     {
+        // The comment below disables the check for unused functions, because while the function is not called directly, it is used by Harmony at runtime.
+        // ReSharper disable once UnusedMember.Local
         private static void Postfix(ref bool __result)
         {
-            if (__result)
-                _connectTime = DateTime.Now;
+            if (__result) // If connected successfully
+                connectTime = DateTime.Now; // Set connect time to current time
         }
     }
-
-    [HarmonyPatch(typeof(NetworkManager), nameof(NetworkManager.StartHost))]
-    internal class HostStartPatch
-    {
-        private static void Postfix(ref bool __result)
-        {
-            if (__result)
-                _connectTime = DateTime.Now;
-        }
-    }
-
-    private static void Print(string value) => ChatCommandAPI.ChatCommandAPI.Print(value);
 }
